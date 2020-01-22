f9308929b30b74aa17ae3dd95c174176218f7a03
nickzylstra <nick.zylstra@gmail.com>
2020-01-22 15:09:06 -0800

    Outline logic

--- index.js
+++ index.js
@@ -5,8 +5,46 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Are you ready for tic-tac-toe??? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const boardData = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const promptMove = () => new Promise((resolve) => {
+  rl.question('Enter 1-9 for your next move ', (move) => {
+    resolve(move);
+  });
+});
+
+const updateBoard = (move) => {
+
+};
+
+const displayBoard = (board) => {
+  
+};
+
+const getGameStatus = (board) => {
+
+  return true;
+};
+
+
+const main = async (board) => {
+  displayBoard(board);
+  let isWin = false;
+
+  while (!isWin) {
+    // eslint-disable-next-line no-await-in-loop
+    const move = await promptMove();
+    updateBoard(move);
+    displayBoard(board);
+
+    isWin = getGameStatus(board);
+  }
 
   rl.close();
-});
+};
+
+main(boardData);
