dd423d531f9f7816eee0757ee5bb845fd4d46c40
nickzylstra <nick.zylstra@gmail.com>
2020-01-22 15:14:12 -0800

    Display board

--- index.js
+++ index.js
@@ -11,18 +11,24 @@ const boardData = [
   [7, 8, 9],
 ];
 
-const promptMove = () => new Promise((resolve) => {
-  rl.question('Enter 1-9 for your next move ', (move) => {
+const promptMove = (player) => new Promise((resolve) => {
+  rl.question(`Player '${player}', enter square (1-9) for your next move: `, (move) => {
     resolve(move);
   });
 });
 
 const updateBoard = (move) => {
-
+  
 };
 
 const displayBoard = (board) => {
-  
+  rl.write(`
+     --------- 
+    | ${board[0][0]}  ${board[0][1]}  ${board[0][2]} |
+    | ${board[1][0]}  ${board[1][1]}  ${board[1][2]} |
+    | ${board[2][0]}  ${board[2][1]}  ${board[2][2]} |
+     ---------
+  `);
 };
 
 const getGameStatus = (board) => {
